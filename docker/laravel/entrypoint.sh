c0691b6647c58c4bc8b227141969f647503e6d6d
Thomas Price <pablo@cbrz.com>
2026-04-10 12:53:17 -0400

    fix - updating local dev environment to run without manual commands

--- docker/laravel/entrypoint.sh
+++ docker/laravel/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
 
+if [ ! -f ".env" ]; then
+    composer run setup
+fi
+
 if [ ! -d "vendor" ] || [ -z "$(ls -A vendor 2>/dev/null)" ] || [ "composer.json" -nt "vendor" ]; then
     echo "Running composer install..."
     composer install
-
 else
     echo "composer packages up to date, skipping composer install"
 fi
+
+
 
 exec "$@"
